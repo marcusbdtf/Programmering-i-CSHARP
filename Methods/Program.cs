ecc473ed9dbd3eddb171c624e8e13da597cbb4f2
marcusbdtf <marcus.hedlund@hotmail.com>
2022-09-09 15:49:12 +0200

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+void MyPrintFunc(string name, int age)
+{
+    Console.WriteLine($"Hey, my name is, {name}. I am {age} years old.");
+};
+
+MyPrintFunc("Marcus", 23);
+
+
+// params for in
+
+
+int myIf(bool returnA, int a, int b)
+{
+    if (returnA)
+    {
+        return a;
+    }
+
+    return b;
+}
+
+Console.WriteLine();
---
+++ Programmering i CSHARP/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Enter size of square");
+
+int Size;
+
+Size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+for (int row = 0; row < Size; row++)
+{
+    for (int column = 0; column < Size; column++)
+    {
+        if (row == 0 || column == 0 || row == Size - 1|| column == Size - 1)
+        {
+            Console.Write("* ");
+        }
+        else
+        {
+            Console.Write("  ");
+        }
+    }
+    Console.WriteLine();
+}
+
+
+
+// ---------------------------------------------------
+Random rnd = new Random();
+
+int num = rnd.Next(100) + 1;
+Console.WriteLine(num);
+// ---------------------------------------------------
+
+int Count = 1;
+int Sum = 0;
+
+while(Count<1001)
+{
+    Sum = Sum + Count; 
+    Count++;
+    Console.WriteLine(Sum);
+}
+
+// ---------------------------------------------------
